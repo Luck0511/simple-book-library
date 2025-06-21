ea0f37d3f737b70ba53d304e708e9a0c389a9037
Luc <lucaterra05@gmail.com>
2025-06-21 14:45:06 +0200

    Initial commit, adding all project files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1>SBL</h1>
+                <h2>Simple Book Library</h2>
+            </div>
+        </>
+    )
+}
+
+export default App
