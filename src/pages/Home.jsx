caf70c06544eb154b5b85b0ecb6c70fc4b647b6c
Luc <lucaterra05@gmail.com>
2025-08-01 18:29:58 +0200

    created home page (empty only header)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import '/src/assets/styleSheets/Home.css'
+
 const Home = () => {
     return (
-        <>
-        </>
+        <div className="heroBody">
+            <section className="title">
+                <section className="text">
+                    <h2>Welcome to SBL</h2>
+                    <h3>an online <br/><i>Simple Book Library</i></h3>
+                </section>
+                <img src="/logo/FavIcon.svg" alt="logo" className="backImage"/>
+            </section>
+        </div>
     )
 }
 export default Home;
